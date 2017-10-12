8c065715fa05a36dc880a6f70898bd43153b75a9
scott <scott@scott.com>
2017-10-12 18:02:09 +0900

    first Commit

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello{
+	public static void main(String[] args){
+		System.out.println("Hello Git world");
+		System.out.println("second branch에서 작업함");	
+
+		System.out.println("3번째 작업을 추가");	
+
+		같은라인에 충돌을 확인하기위해 추가합니다.
+
+	}
+}
